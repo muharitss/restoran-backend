f169830e365b0f2671ef28e966cf51b967681a56
muharits <wert640559@gmail.com>
2026-02-12 10:50:51 +0700

    feat: Initialize Express server with CORS, environment configuration, and core API routes.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,43 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes';
-import menuRoutes from './routes/menu.routes';
-import categoriesRoutes from './routes/category.routes';
-import orderRoutes from './routes/order.routes';
-import paymentRoutes from './routes/payment.routes';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoutes from "./routes/user.routes";
+import menuRoutes from "./routes/menu.routes";
+import categoriesRoutes from "./routes/category.routes";
+import orderRoutes from "./routes/order.routes";
+import paymentRoutes from "./routes/payment.routes";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); 
+
+const allowedOrigins = [
+  "https://restorana.netlify.app",
+  "http://localhost:5173",
+  "https://restoran-app-five.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
 
 // Daftarkan route
-app.use('/api/users', userRoutes);
-app.use('/menus', menuRoutes);
-app.use('/categories', categoriesRoutes);
-app.use('/orders', orderRoutes);
-app.use('/payments', paymentRoutes);
+app.use("/api/users", userRoutes);
+app.use("/menus", menuRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/orders", orderRoutes);
+app.use("/payments", paymentRoutes);
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
